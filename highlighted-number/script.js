06aa9991c06864eb315128fe502d5d6e9e5b65e6
sofia amoroso <sofi79a0@stud.kea.dk>
2024-09-10 12:50:17 +0200

    small improvements

--- highlighted-number/script.js
+++ highlighted-number/script.js
@@ -12,18 +12,16 @@ numbers.forEach(x =>{
     div.textContent = x;
     container.appendChild(div);
     div.classList.add("number");
-    isEven(x, div);
+    div.classList.add(x % 2 === 0 ? "even" : "odd"); //note to myself: remember to use the ternary operator >:(
 })
 
-// Check if the number is even or odd and apply the appropriate class
 
-function isEven(x, div){
-    if (x % 2 == 0){
-        div.classList.add("even")
-    } else {
-        div.classList.add("odd")
+container.addEventListener("click", (e) => {
+    if (e.target && e.target.classList.contains("number")) {
+        changeColour(e.target);  // Only change color for clicked numbers
     }
-}
+});
+
 
 // Add an event listener to highlight the number when clicked
 
@@ -39,14 +37,13 @@ Array.from(numbersDivs).forEach(div => {
 // Remove highlight from all numbers
 // Add highlight to the clicked number
 
-function changeColour(div) {
-    if( div.classList.contains("highlight")){
-        div.classList.remove("highlight");  
-    } else {
-        div.classList.add("highlight"); 
+function changeColour(clickedDiv) {
+    const highlighted = container.querySelector(".highlight");
+    if (highlighted) {
+        highlighted.classList.remove("highlight");
     }
+    clickedDiv.classList.add("highlight");
 }
-
     
 
     
