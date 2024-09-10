2e05686ccb81857fae8c170506696f3cd06cde81
sofia amoroso <sofi79a0@stud.kea.dk>
2024-09-10 11:41:24 +0200

    start of exercise

---
+++ highlighted-number/script.js
@@ -0,0 +1,18 @@
+// Get the container element
+
+
+// Loop through numbers from 1 to 20
+
+    // Create a new div element for each number
+    
+    // Check if the number is even or odd and apply the appropriate class
+    
+    // Add an event listener to highlight the number when clicked
+
+        // Remove highlight from all numbers
+        
+        // Add highlight to the clicked number
+    
+    // Append the new div to the container
+    
+
